49762357313289bc2d7cf86571b4c2a4d6af620e
Guilherme Romeiro <romeiro.gui@hotmail.com>
2024-02-20 13:33:08 -0300

    Subindo mais um conteudo parte de novidades

--- src/pages/Novidades.jsx
+++ src/pages/Novidades.jsx
@@ -1,65 +1,70 @@
-import styles from "../pages/Novidades.module.css"
-import celular from "../imgs/CelularPlataformas.png"
+import styles from "../pages/Novidades.module.css";
+import celular from "../imgs/CelularPlataformas.png";
 import Qrcode from "../imgs/QrcodeWinbank.png";
-
-function Novidades(){
-    return(
-        <>
-                <main className={styles.Fundo}>
-
-           
-             <div className={styles.TituloNovidadesPrincipal}>
+import CartaoDireito from "../imgs/CartoesDireito.png";
+import CartaoEsquerdo from "../imgs/CartoesEsquerdo.png";
 
 
-                <div className={styles.TituloNovidades}>
-                
-                <h1>Novidades chegando para o seu celular, fique ligado!</h1>
-  
-                </div>
+function Novidades() {
+  return (
+    <>
+      <main className={styles.ContainerPersonalizePrincipal}>
+        <section className={styles.PerzonalizePrincipal}>
+          <div className={styles.TextoPerzonalizePrincipal}>
+            <p className={styles.TextoPerzonalize}>Personalize seu Cartão sem anuidade</p>
+          </div>
 
-              </div>   
+          <div className={styles.CartaoDireitoPrincipal}>
+            <img className={styles.imgCartaoDireito} src={CartaoEsquerdo} alt="" />
+          </div> 
+        </section>
 
-              <div className={styles.CelularPrincipal}> 
+        <aside className={styles.CartaoEsquerdoPrincipal}>
+            <div></div>
 
-                
-                <div className={styles.Celular}>
-
-                    <img className={styles.ImgCelular} src={celular} alt="" />
-
-                </div>
-  
-             </div>   
-   
-
-        </main>
-
-        <main className={styles.QrcodeWinbank}>
-
-            <div className={styles.ContainerTexto}>
-
-                <p className={styles.TextoQrcode}>O WinApp recomenda que você baixe 
-                nosso aplicativo parceiro para uma gestão ainda mais eficiente do 
-                seu saldo e cartões, garantindo que você tenha sempre as melhores 
-                ferramentas financeiras ao seu alcance.</p>
-
+            <div>
+                <img className={styles.ImgCartaoEsquerdo} src={CartaoDireito} alt="" />
             </div>
 
-            <div className={styles.ContainerQrcodeImg}>
-                
-                <div className={styles.ContainerImg}>
-                
-                    <img className={styles.QrcodeImg} src={Qrcode} alt="Qrcode mobile" />
+        </aside>
+      </main>
 
-                </div>
+      <main className={styles.Fundo}>
+        <div className={styles.TituloNovidadesPrincipal}>
+          <div className={styles.TituloNovidades}>
+            <h1>Novidades chegando para o seu celular, fique ligado!</h1>
+          </div>
+        </div>
 
-            </div>
+        <div className={styles.CelularPrincipal}>
+          <div className={styles.Celular}>
+            <img className={styles.ImgCelular} src={celular} alt="" />
+          </div>
+        </div>
+      </main>
 
+      <main className={styles.QrcodeWinbank}>
+        <div className={styles.ContainerTexto}>
+          <p className={styles.TextoQrcode}>
+            O WinApp recomenda que você baixe nosso aplicativo parceiro para uma
+            gestão ainda mais eficiente do seu saldo e cartões, garantindo que
+            você tenha sempre as melhores ferramentas financeiras ao seu
+            alcance.
+          </p>
+        </div>
 
-        </main>
-
-        
-        </>
-    );
+        <div className={styles.ContainerQrcodeImg}>
+          <div className={styles.ContainerImg}>
+            <img
+              className={styles.QrcodeImg}
+              src={Qrcode}
+              alt="Qrcode mobile"
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Novidades;
