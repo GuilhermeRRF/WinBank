b796401065f9a9bf10b72611e6580bd13bbc1f83
Guilherme Romeiro <romeiro.gui@hotmail.com>
2024-02-26 12:13:25 -0300

    subindo segunda parte do perfil pessoal

--- src/pages/PerfilPessoal.jsx
+++ src/pages/PerfilPessoal.jsx
@@ -1,50 +1,121 @@
 import styles from "../pages/PerfilPessoal.module.css";
 import FotoPerfil from "../imgs/FotoPerfilPessoal.png";
 
-function PerfilPessoal(){
-    return(
-
-
-        <>
-        <main className={styles.PaginaPessoalPrincipal}>
-
+function PerfilPessoal() {
+  return (
+    <>
+      <main className={styles.PaginaPessoalPrincipal}>
         <div className={styles.FotoPerfilPrincipal}>
-
-            <img className={styles.FotoPerfil} src={FotoPerfil} alt="foto pessoal do usuario" />
-            
+          <img
+            className={styles.FotoPerfil}
+            src={FotoPerfil}
+            alt="foto pessoal do usuario"
+          />
         </div>
 
         <div className={styles.PerfilDadosPessoais}>
-            <div className={styles.TituloPerfilPrincipal}>
+          <div className={styles.TituloPerfilPrincipal}>
             <p className={styles.TituloPerfil}>Guilherme Romeiro</p>
-            </div>
-           
-            <div className={styles.MiniDadosPerfilPrincipal}>
+          </div>
+
+          <div className={styles.MiniDadosPerfilPrincipal}>
             <div className={styles.MiniDadosPerfil}>
-            <p className={styles.Perfil}>São Bernabel</p><p className={styles.Perfil}>Tel: 951561686</p>
+              <p className={styles.Perfil}>São Bernabel</p>
+              <p className={styles.Perfil}>Tel: 951561686</p>
             </div>
 
             <div className={styles.MiniDadosPerfil2}>
-            <p className={styles.Perfil2}>21 anos</p> <p className={styles.Perfil2}>Tipo de Conta: Corrente</p>
+              <p className={styles.Perfil2}>21 anos</p>{" "}
+              <p className={styles.Perfil2}>Tipo de Conta: Corrente</p>
             </div>
-            </div>
+          </div>
+        </div>
+      </main>
+
+      <div className={styles.linha}></div>
+
+      <div className="TituloPrincipal">
+        <p className={styles.Titulo}> Dados Cadastrais</p>
+      </div>
+
+      <section className={styles.PaginaCadastralPrincipal}>
+        <div className={styles.FormularioDireito}>
+          <form className={styles.ContainerFormulario}>
+            <input
+              className={styles.campo}
+              type="text"
+              name="nomeCompleto"
+              placeholder="Nome Completo"
+            />
+
+            <input
+              className={styles.campo}
+              type="text"
+              name="celular"
+              placeholder="Celular"
+            />
+
+            <input
+              className={styles.campo}
+              type="text"
+              name="endereco"
+              placeholder="Endereço"
+            />
+
+            <input
+              className={styles.campo}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </form>
         </div>
 
-        </main>
-        <div className={styles.linha}>
+        <div className={styles.FormularioEsquerdo}>
+          <form className={styles.ContainerFormulario2}>
+            <input
+              className={styles.campo}
+              type="text"
+              name="cpf"
+              placeholder="CPF"
+            />
 
+            <select
+              className={styles.campo}
+              name="accountType"
+              id="accountType"
+            >
+              <option value="corrente">Corrente</option>
+              <option value="poupanca">Conta Poupança</option>
+              <option value="kids">Conta Kids</option>
+            </select>
+
+            <input className={styles.campo} type="date" name="dataNascimento" />
+
+            <input
+              className={styles.campo}
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="Senha"
+              required
+            />
+          </form>
         </div>
-        <section className={styles.PaginaCadastralPrincipal}>
-
-
-            Dados Cadastrais
-
-
-        </section>
-        </>
-
-
-    );
+      </section>
+      
+      <div className={styles.BotaoInferior}>
+              <button className={styles.BotaoFormulario2} type="submit">
+              Alterar Informações
+              </button>
+              <button className={styles.BotaoFormulario} type="submit">
+                Sair
+              </button>
+            </div>
+    </>
+  );
 }
 
 export default PerfilPessoal;
